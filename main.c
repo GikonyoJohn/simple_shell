0aa496bc899f8b3c0783c7e02eefe1ff30e30325
GikonyoJohn <gikonyojohn39@gmail.com>
2022-05-18 21:32:27 +0300

    0x016 Simple_shell

--- main.c
+++ main.c
@@ -1,29 +1,73 @@
 #include "main.h"
 
 /**
- * main - start shell.
+ * free_data - frees data structure
  *
- * Return: (0).
+ * @datash: data structure
+ * Return: no return
  */
-
-int main(void)
+void free_data(data_shell *datash)
 {
-	char *row = NULL;
-	char **tokens = NULL;
-	int ct_output = 0;
+	unsigned int i;
 
-	while (1)
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		if (isatty(STDIN))
-			write(STDOUT, "#holbies$ ", 10);
-
-		row = rd_row();
-		tokens = tokenizer(row);
-		ct_output = Run(tokens, ct_output, row);
-
-		free(row);
-		free(tokens);
+		free(datash->_environ[i]);
 	}
 
-	return (0);
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
